2c9db0a30784f34eeed4bbe92c0ecd78e9b8bc9e
helme <helme@posteo.de>
2012-11-08 23:37:19 +0100

    navigation ab working-dir

--- src/gui/NavigationPanel.java
+++ src/gui/NavigationPanel.java
@@ -1,14 +1,68 @@
 package gui;
 
+import java.io.File;
+import java.util.Vector;
+import java.util.Collections;
+
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 public class NavigationPanel extends JPanel {
 	
-	private JTree mainNav;
+	private JTree tree;
+	private File f;
 
 	public NavigationPanel() {
-		mainNav= new JTree();
+		f = new File(".");
+		tree =  new JTree(addNodes(null,f));
+		
+		   tree.addTreeSelectionListener(new TreeSelectionListener() {
+			      public void valueChanged(TreeSelectionEvent e) {
+			        DefaultMutableTreeNode node = (DefaultMutableTreeNode) e
+			            .getPath().getLastPathComponent();
+			        System.out.println("You selected " + node);
+			      }
+			    });
+		add(tree);
+		 
+		 
 	}
+	
+	private DefaultMutableTreeNode addNodes(DefaultMutableTreeNode curTop, File dir) {
+	    String curPath = dir.getPath();
+	    DefaultMutableTreeNode curDir = new DefaultMutableTreeNode(curPath);
+	    if (curTop != null) { // should only be null at root
+	      curTop.add(curDir);
+	    }
+	    Vector ol = new Vector();
+	    String[] tmp = dir.list();
+	    for (int i = 0; i < tmp.length; i++)
+	      ol.addElement(tmp[i]);
+	    Collections.sort(ol, String.CASE_INSENSITIVE_ORDER);
+	    File f;
+	    Vector files = new Vector();
+	    // Make two passes, one for Dirs and one for Files. This is #1.
+	    for (int i = 0; i < ol.size(); i++) {
+	      String thisObject = (String) ol.elementAt(i);
+	      String newPath;
+	      if (curPath.equals("."))
+	        newPath = thisObject;
+	      else
+	        newPath = curPath + File.separator + thisObject;
+	      if ((f = new File(newPath)).isDirectory())
+	        addNodes(curDir, f);
+	      else
+	        files.addElement(thisObject);
+	    }
+	    // Pass two: for files.
+	    for (int fnum = 0; fnum < files.size(); fnum++)
+	      curDir.add(new DefaultMutableTreeNode(files.elementAt(fnum)));
+	    return curDir;
+	  }
 
 }
