eb47bee64dd0996de86e5cf06229b3d2a8ccbc32
helme <helme@posteo.de>
2012-11-09 01:55:42 +0100

    Navigation

--- src/gui/NavigationPanel.java
+++ src/gui/NavigationPanel.java
@@ -1,9 +1,13 @@
 package gui;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.io.File;
 import java.util.Vector;
 import java.util.Collections;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTree;
@@ -15,28 +19,29 @@ import javax.swing.tree.DefaultTreeModel;
 public class NavigationPanel extends JPanel {
 	
 	private JTree tree;
-	private File f;
+	private File file;
 
 	public NavigationPanel() {
-		f = new File(".");
-		tree =  new JTree(addNodes(null,f));
-		
-		   tree.addTreeSelectionListener(new TreeSelectionListener() {
+		setLayout(new GridLayout(1,1));
+		file = new File(".");
+		tree =  new JTree(addNodes(null,file));
+		tree.addTreeSelectionListener(new TreeSelectionListener() {
 			      public void valueChanged(TreeSelectionEvent e) {
 			        DefaultMutableTreeNode node = (DefaultMutableTreeNode) e
 			            .getPath().getLastPathComponent();
 			        System.out.println("You selected " + node);
 			      }
 			    });
-		add(tree);
-		 
-		 
+		add(tree);	 
+		
+		
+		
 	}
 	
 	private DefaultMutableTreeNode addNodes(DefaultMutableTreeNode curTop, File dir) {
 	    String curPath = dir.getPath();
 	    DefaultMutableTreeNode curDir = new DefaultMutableTreeNode(curPath);
-	    if (curTop != null) { // should only be null at root
+	    if (curTop != null) { 
 	      curTop.add(curDir);
 	    }
 	    Vector ol = new Vector();
@@ -46,7 +51,6 @@ public class NavigationPanel extends JPanel {
 	    Collections.sort(ol, String.CASE_INSENSITIVE_ORDER);
 	    File f;
 	    Vector files = new Vector();
-	    // Make two passes, one for Dirs and one for Files. This is #1.
 	    for (int i = 0; i < ol.size(); i++) {
 	      String thisObject = (String) ol.elementAt(i);
 	      String newPath;
@@ -59,10 +63,9 @@ public class NavigationPanel extends JPanel {
 	      else
 	        files.addElement(thisObject);
 	    }
-	    // Pass two: for files.
 	    for (int fnum = 0; fnum < files.size(); fnum++)
 	      curDir.add(new DefaultMutableTreeNode(files.elementAt(fnum)));
+	    System.out.println(curDir);
 	    return curDir;
 	  }
-
 }
