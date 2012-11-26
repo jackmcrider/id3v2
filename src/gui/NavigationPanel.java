4dcdf59a35f9023169cd56e1763cb29860eb2f16
Karl-Philipp Wulfert <animungo@gmail.com>
2012-11-26 18:26:58 +0100

    Add classes for folders and files

--- src/gui/NavigationPanel.java
+++ src/gui/NavigationPanel.java
@@ -1,13 +1,6 @@
 package gui;
 
 import java.awt.GridLayout;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Vector;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -16,10 +9,11 @@ import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+import model.Folder;
+
 public class NavigationPanel extends JScrollPane {
 	private static final long serialVersionUID = -7186448742695743778L;
 	private JTree tree;
-	private File file;
 	private JPanel panel;
 
 	/*
@@ -31,8 +25,7 @@ public class NavigationPanel extends JScrollPane {
 		panel = new JPanel();
 		panel.setLayout(new GridLayout(1, 1));
 
-		file = new File("mp3s");
-		tree = new JTree(createTree(null, file));
+		tree = new JTree(new Folder(null));
 
 		tree.addTreeSelectionListener(new TreeSelectionListener() {
 			public void valueChanged(TreeSelectionEvent e) {
@@ -49,46 +42,32 @@ public class NavigationPanel extends JScrollPane {
 	}
 
 	/*
-	 * DefaultMutableTreeNode greates the file tree by creating nodes from files
-	 * and directories
+	 * private DefaultMutableTreeNode createTree(DefaultMutableTreeNode curTop,
+	 * File dir) {
+	 * 
+	 * String curPath = dir.getPath(); DefaultMutableTreeNode curDir = new
+	 * DefaultMutableTreeNode(curPath);
+	 * 
+	 * if (curTop != null) { curTop.add(curDir); }
+	 * 
+	 * Vector vec = new Vector(); String[] tmp = dir.list();
+	 * 
+	 * for (int i = 0; i < tmp.length; i++) vec.addElement(tmp[i]);
+	 * 
+	 * Collections.sort(vec, String.CASE_INSENSITIVE_ORDER); File file; Vector
+	 * files = new Vector();
+	 * 
+	 * for (int i = 0; i < vec.size(); i++) { String thisObject = (String)
+	 * vec.elementAt(i); String newPath;
+	 * 
+	 * if (curPath.equals(".")) newPath = thisObject; else newPath = curPath +
+	 * File.separator + thisObject; if ((file = new
+	 * File(newPath)).isDirectory()) createTree(curDir, file); else
+	 * files.addElement(thisObject); }
+	 * 
+	 * for (int fnum = 0; fnum < files.size(); fnum++) curDir.add(new
+	 * DefaultMutableTreeNode(files.elementAt(fnum)));
+	 * 
+	 * return curDir; }
 	 */
-	private DefaultMutableTreeNode createTree(DefaultMutableTreeNode curTop,
-			File dir) {
-
-		String curPath = dir.getPath();
-		DefaultMutableTreeNode curDir = new DefaultMutableTreeNode(curPath);
-
-		if (curTop != null) {
-			curTop.add(curDir);
-		}
-
-		Vector vec = new Vector();
-		String[] tmp = dir.list();
-
-		for (int i = 0; i < tmp.length; i++)
-			vec.addElement(tmp[i]);
-
-		Collections.sort(vec, String.CASE_INSENSITIVE_ORDER);
-		File file;
-		Vector files = new Vector();
-
-		for (int i = 0; i < vec.size(); i++) {
-			String thisObject = (String) vec.elementAt(i);
-			String newPath;
-
-			if (curPath.equals("."))
-				newPath = thisObject;
-			else
-				newPath = curPath + File.separator + thisObject;
-			if ((file = new File(newPath)).isDirectory())
-				createTree(curDir, file);
-			else
-				files.addElement(thisObject);
-		}
-
-		for (int fnum = 0; fnum < files.size(); fnum++)
-			curDir.add(new DefaultMutableTreeNode(files.elementAt(fnum)));
-
-		return curDir;
-	}
 }
--- src/model/Folder.java
+++ src/model/Folder.java
@@ -1,7 +1,29 @@
 package model;
 
+import java.io.File;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class Folder extends DefaultMutableTreeNode {
+	private static final long serialVersionUID = -9065207958966277512L;
+	private File representedFolder;
 
+	public Folder(String path) {
+		if (path == null)
+			path = "mp3s";
+
+		representedFolder = new File(path);
+
+		for (File f : representedFolder.listFiles()) {
+			if (f.isDirectory()) {
+				this.add(new Folder(f.getPath()));
+			} else {
+				this.add(new MP3File(f.getPath()));
+			}
+		}
+	}
+
+	public String toString() {
+		return representedFolder.getName();
+	}
 }
