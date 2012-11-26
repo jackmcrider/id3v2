662c1e69c9236fb59f01c0a284885c6cd7cfe5f3
Karl-Philipp Wulfert <animungo@gmail.com>
2012-11-26 22:36:16 +0100

    Added counter to folders

--- src/model/Folder.java
+++ src/model/Folder.java
@@ -7,6 +7,7 @@ import javax.swing.tree.DefaultMutableTreeNode;
 public class Folder extends DefaultMutableTreeNode {
 	private static final long serialVersionUID = -9065207958966277512L;
 	private File representedFolder;
+	private int amountOfChildren = 0;
 
 	public Folder(String path) {
 		if (path == null)
@@ -20,10 +21,11 @@ public class Folder extends DefaultMutableTreeNode {
 			} else {
 				this.add(new MP3File(f.getPath()));
 			}
+			this.amountOfChildren++;
 		}
 	}
 
 	public String toString() {
-		return this.representedFolder.getName();
+		return this.representedFolder.getName() + " (" + this.amountOfChildren + ")";
 	}
 }
