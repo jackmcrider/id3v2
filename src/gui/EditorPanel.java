1b08412a96863be2333ffb077e3180b5002490db
Karl-Philipp Wulfert <animungo@gmail.com>
2012-11-26 19:08:23 +0100

    Organize imports

--- src/gui/EditorPanel.java
+++ src/gui/EditorPanel.java
@@ -11,8 +11,6 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
