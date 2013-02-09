d600904e78e161fada4546018738565a49e07ecf
Jacob Kauffmann <jackmcrider@gmail.com>
2013-02-09 18:27:31 +0100

    paar tests

---
+++ src/tests/control.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class control
+{
+	private
+	
+	@BeforeClass
+	public static void init()
+	{
+		
+	}
+	
+	@Test
+	public void test()
+	{
+		
+	}
+
+}
