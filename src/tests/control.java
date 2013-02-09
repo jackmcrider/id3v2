3b364f3ff86619b997d2660216da60c552aca9c4
Jacob Kauffmann <jackmcrider@gmail.com>
2013-02-09 19:24:27 +0100

    mp3 nach .mp3

--- src/tests/control.java
+++ src/tests/control.java
@@ -2,23 +2,30 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import model.MP3File;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import view.EditorPanel;
+import view.MainWindow;
+import view.NavigationPanel;
+import control.MainControl;
+
 public class control
 {
-	private
+	private static MainControl tagger;
 	
 	@BeforeClass
 	public static void init()
 	{
-		
+		tagger = new MainControl();
 	}
 	
 	@Test
 	public void test()
 	{
-		
+		System.out.println(tagger.getChangedFiles());
 	}
 
 }
